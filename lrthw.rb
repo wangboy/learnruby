ba518f381308baaf0e6ff03b364750848f194c4c
wangboy <bjutwangboy@gmail.com>
2015-10-28 19:04:28 +0800

    exe19

--- lrthw.rb
+++ lrthw.rb
@@ -383,10 +383,39 @@ def print_none()
 end
 
 
-print_two("Zed","Shaw")
-print_two_again("Zed","Shaw")
+print_two("Zed", "Shaw")
+print_two_again("Zed", "Shaw")
 print_one("First!")
 print_none()
 print_none
 
 
+#Exercise 19: Functions and Variables
+
+puts "\n exe 19"
+
+def cheese_and_crackers(cheese_count, boxes_of_crackers)
+  puts "You have #{cheese_count} cheeses!"
+  puts "You have #{boxes_of_crackers} boxes of crackers!"
+  puts "Man that's enough for a party!"
+  puts "Get a blanket.\n"
+end
+
+
+puts "We can just give the function numbers directly:"
+cheese_and_crackers(20, 30)
+
+
+puts "OR, we can use variables from our script:"
+amount_of_cheese = 10
+amount_of_crackers = 50
+
+cheese_and_crackers(amount_of_cheese, amount_of_crackers)
+
+
+puts "We can even do math inside too:"
+cheese_and_crackers(10 + 20, 5 + 6)
+
+
+puts "And we can combine the two, variables and math:"
+cheese_and_crackers(amount_of_cheese + 100, amount_of_crackers + 1000)
