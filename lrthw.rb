8fcc63390b596741f3921b279c58ca5d0e18e2eb
wangboy <bjutwangboy@gmail.com>
2015-10-29 08:31:27 +0800

    ex25

--- lrthw.rb
+++ lrthw.rb
@@ -110,7 +110,7 @@ puts ""
 puts "Mary had a little lamb."
 puts "Its fleece was white as #{'snow'}."
 puts "And everywhere that Mary went."
-puts "." * 10 # what'd that do?
+puts "." * 10 # what'd that do?       ################
 
 end1 = "C"
 end2 = "h"
@@ -134,9 +134,9 @@ puts end7 + end8 + end9 + end10 + end11 + end12
 
 puts "\n"
 
-formatter = "%{first} %{second} %{third} %{fourth}"
+formatter = "%{first} %{second} %{third} %{fourth}"        ################
 
-puts formatter % {first: 1, second: 2, third: 3, fourth: 4}
+puts formatter % {first: 1, second: 2, third: 3, fourth: 4}      ################
 puts formatter % {second: "two", first: "one", third: "three", fourth: "four"}
 puts formatter % {first: true, second: false, third: true, fourth: false}
 puts formatter % {first: formatter, second: formatter, third: formatter, fourth: formatter}
@@ -159,12 +159,13 @@ months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
 puts "Here are the days: #{days}"
 puts "Here are the months: #{months}"
 
-puts "" "
+                                                #############
+puts """       
 There's something going on here.
 With the three double-quotes.
 We'll be able to type as much as we like.
 Even 4 lines if we want, or 5, or 6.
-" ""
+"""
 
 
 puts "\nExercise 10: What Was That?"
@@ -199,14 +200,14 @@ puts fat_cat
 #Exercise 11: Asking Questions
 
 # print "How old are you? "
-# age = gets.chomp
+# age = gets.chomp            #############
 # print "How tall are you? "
 # height = gets.chomp
 # print "How much do you weigh? "
 # weight = gets.chomp
 #
 #
-# more = gets.chomp.to_i
+# more = gets.chomp.to_i                #############
 #
 # printf "abc"
 #
@@ -235,7 +236,7 @@ puts fat_cat
 
 #Exercise 13: Parameters, Unpacking, Variables
 
-first, second, third = ARGV
+first, second, third = ARGV               #############
 
 puts "Your first variable is: #{first}"
 puts "Your second variable is: #{second}"
@@ -250,7 +251,7 @@ puts "Your third variable is: #{third}"
 # puts "Hi #{user_name}."
 # puts "I'd like to ask you a few questions."
 # puts "Do you like me #{user_name}? ", prompt
-# likes = $stdin.gets.chomp
+# likes = $stdin.gets.chomp               #############
 #
 # puts "Where do you live #{user_name}? ", prompt
 # lives = $stdin.gets.chomp
@@ -270,19 +271,19 @@ puts "Your third variable is: #{third}"
 puts "\n exe 15"
 
 # filename = ARGV.first
-filename = "rt15.txt"
+filename = "rt15"
 
 prompt = "> "
-txt = File.open(filename)
+txt = File.open(filename)               #############
 
 puts "Here's your file: #{filename}"
-puts txt.read
+puts txt.read               #############
 
 txt.close
 
 # puts "Type the filename again:"
 # print prompt
-# file_again = STDIN.gets.chomp()
+# file_again = STDIN.gets.chomp()               #############
 #
 # txt_again = File.open(file_again)
 #
@@ -303,7 +304,7 @@ puts "\n exe 16"
 # $stdin.gets
 #
 # puts "Opening the file..."
-# target = open(filename, 'w')
+# target = open(filename, 'w')               #############
 #
 # puts "Truncating the file.  Goodbye!"
 # target.truncate(0)
@@ -347,7 +348,7 @@ puts "The input file is #{indata.length} bytes long"
 
 puts "Does the output file exist? #{File.exist?(to_file)}"
 puts "Ready, hit RETURN to continue, CTRL-C to abort."
-$stdin.gets
+# $stdin.gets               #############
 
 out_file = open(to_file, 'w')
 out_file.write(indata)
@@ -361,7 +362,7 @@ in_file.close
 #Exercise 18: Names, Variables, Code, Functions
 puts "\n exe 18"
 
-# this one is like your scripts with ARGV
+# this one is like your scripts with ARGV               #############
 def print_two(*args)
   arg1, arg2 = args
   puts "arg1: #{arg1}, arg2: #{arg2}"
@@ -421,5 +422,184 @@ puts "And we can combine the two, variables and math:"
 cheese_and_crackers(amount_of_cheese + 100, amount_of_crackers + 1000)
 
 puts " end "
+
+
+#Exercise 20: Functions and Files
+
+puts "\n exe 20"
+
+# input_file = ARGV.first
+input_file = "r20"
+
+def print_all(f)
+  puts f.read
+end
+
+def rewind(f)
+  f.seek(0)               #############
+end
+
+def print_a_line(line_count, f)
+  puts "#{line_count}, #{f.gets.chomp}"               #############
+end
+
+current_file = open(input_file)
+
+puts "First let's print the whole file:\n"
+
+print_all(current_file)
+
+puts "Now let's rewind, kind of like a tape."
+
+rewind(current_file)
+
+puts "Let's print three lines:"
+
+current_line = 1
+print_a_line(current_line, current_file)
+
+current_line = current_line + 1
+print_a_line(current_line, current_file)
+
+current_line = current_line + 1
+print_a_line(current_line, current_file)
+
+
+
+#Exercise 21: Functions Can Return Something
+puts "\n exe 21"
+
+def add(a, b)
+  puts "ADDING #{a} + #{b}"
+  return a + b               #############
+end
+
+def subtract(a, b)
+  puts "SUBTRACTING #{a} - #{b}"
+  return a - b
+end
+
+def multiply(a, b)
+  puts "MULTIPLYING #{a} * #{b}"
+  return a * b
+end
+
+def divide(a, b)
+  puts "DIVIDING #{a} / #{b}"
+  return a / b
+end
+
+
+puts "Let's do some math with just functions!"
+
+age = add(30, 5)
+height = subtract(78, 4)
+weight = multiply(90, 2)
+iq = divide(100, 2)
+
+puts "Age: #{age}, Height: #{height}, Weight: #{weight}, IQ: #{iq}"
+
+
+# A puzzle for the extra credit, type it in anyway.
+puts "Here is a puzzle."
+
+what = add(age, subtract(height, multiply(weight, divide(iq, 2))))
+
+puts "That becomes: #{what}. Can you do it by hand?"
+
+def noreturn()
+  a = 1 + 2               #############
+end
+
+nr = noreturn
+
+puts "[#{nr}]"
+
+
+#Exercise 22: What Do You Know So Far?
+
+puts "\n exe 22"
+
+
+#Exercise 23: Read Some Code
+
+puts "\n exe 23"
+
+
+#Exercise 24: More Practice
+
+puts "\n exe 24"
+
+puts "Let's practice everything."
+puts 'You\'d need to know \'bout escapes with \\ that do \n newlines and \t tabs.'
+
+# the <<END is a "heredoc". See the Student Questions.
+               #############
+poem = <<END                        ################
+\tThe lovely world
+with logic so firmly planted
+cannot discern \n the needs of love
+nor comprehend passion from intuition
+and requires an explanation
+\n\t\twhere there is none.
+END
+
+puts "--------------"
+puts poem
+puts "--------------"
+
+
+five = 10 - 2 + 3 - 6
+puts "This should be five: #{five}"
+
+def secret_formula(started)
+  jelly_beans = started * 500
+  jars = jelly_beans / 1000
+  crates = jars / 100
+  return jelly_beans, jars, crates  ################
+end
+
+
+start_point = 10000
+beans, jars, crates = secret_formula(start_point)    ################
+
+puts "With a starting point of: #{start_point}"
+puts "We'd have #{beans} beans, #{jars} jars, and #{crates} crates."
+
+start_point = start_point / 10
+
+
+#Exercise 25: Even More Practice
+puts "\n exe 25"
+
+require "./ex25.rb"
+
+def printfn(a)
+  print a
+  puts ""
+end
+
+sentence = "All good things come to those who wait."
+words = Ex25.break_words(sentence)
+printfn words
+sorted_words = Ex25.sort_words(words)
+printfn sorted_words
+Ex25.print_first_word(words)
+Ex25.print_last_word(words)
+printfn words
+Ex25.print_first_word(sorted_words)
+Ex25.print_last_word(sorted_words)
+printfn sorted_words
+sorted_words = Ex25.sort_sentence(sentence)
+printfn sorted_words
+Ex25.print_first_and_last(sentence)
+Ex25.print_first_and_last_sorted(sentence)
+
+
+
+
+
+
+
 
 
